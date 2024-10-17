df38fd652820114b3c5e4936500ba06c6bb49778
Hajrahmukthar <hmukhtar2@instituticaria.cat>
2024-10-17 09:19:45 +0200

    Enviament

--- divisio.py
+++ divisio.py
@@ -2,15 +2,15 @@
 Dividend = int(input("Escriu el Dividend (nombre enter): "))
 Divisó = int(input("Escriu el Divisó (nombre enter): "))
 
-# Comprovem que el divisor no sigui zero
+# Comprovem que el Divisó no sigui zero
 if Divisó == 0:
-    print("Error: No es pot calcular la Divisió perquè el divisor no pot ser 0.")
+    print("Error: No es pot calcular la Divisió perquè el Divisó no es pot ser 0.")
 else:
-    # Calculem el Quocient i el Residu
+    # Calculem el Quocient i el Residu 
     Quocient = Dividend // Divisó
-    Residu = Dividend % Divisó
+    Residu = Dividend % Divisó 
 
-    # Mostrem els resultats
+    # Mostrem els resultats 
     print(f"La Divisió és: {Dividend}/{Divisó}")
     print(f"Quocient: {Quocient}")
     print(f"Residu: {Residu}")
